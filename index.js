006e075d2127e7d08f2c228917066898a2794a43
pth-1641 <pth16042001@gmail.com>
2023-01-25 11:26:39 +0700

    Testing

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
+// const express = require('express');
+// const axios = require('axios').default;
+// const dotenv = require('dotenv');
+
+// dotenv.config();
+
+// const app = express();
+// const port = process.env.PORT || 8080;
+// const referer = process.env.REFERER_URL;
+// app.use(express.json());
+
+// app.use('/proxy', async (req, res) => {
+//   try {
+//     const { src } = req.query;
+//     const { data } = await axios({
+//       method: 'GET',
+//       url: src,
+//       responseType: 'stream',
+//       headers: {
+//         Accept: '*/*',
+//         referer,
+//       },
+//     });
+//     return data.pipe(res);
+//   } catch (err) {
+//     res.status(err?.code || 500).json({
+//       message: err?.message || 'Bad request',
+//     });
+//   }
+// });
+
+// app.listen(port);
+
 const express = require('express');
-const axios = require('axios').default;
-const dotenv = require('dotenv');
-
-dotenv.config();
-
 const app = express();
-const port = process.env.PORT || 8080;
-const referer = process.env.REFERER_URL;
-app.use(express.json());
-
-app.use('/proxy', async (req, res) => {
-  try {
-    const { src } = req.query;
-    const { data } = await axios({
-      method: 'GET',
-      url: src,
-      responseType: 'stream',
-      headers: {
-        Accept: '*/*',
-        referer,
-      },
-    });
-    // return data.pipe(res);
-    return res.send('hii');
-  } catch (err) {
-    res.status(err?.code || 500).json({
-      message: err?.message || 'Bad request',
-    });
-  }
+app.all('/', (req, res) => {
+  console.log('Just got a request!');
+  res.send('Yo!');
 });
-
-app.listen(port);
+app.listen(process.env.PORT || 3000);
